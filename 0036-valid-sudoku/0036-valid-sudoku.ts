147b6d56a9910fe086ac2fe12652e65f4a17a879
Minhee-Jeon <58028527+Minhee-Jeon@users.noreply.github.com>
2025-07-01 19:34:37 +0900

    Time: 3 ms (88.06%), Space: 57.8 MB (93.91%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.ts
+++ 0036-valid-sudoku/0036-valid-sudoku.ts
@@ -1,25 +1,25 @@
 function isValidSudoku(board: string[][]): boolean {
-     const row = new Array(9).fill(null).map(() => new Set([])), 
-    	col = new Array(9).fill(null).map(() => new Set([])), 
-        box = new Array(9).fill(null).map(() => new Set([]));
+     for(let i =0; i<board.length;i++){
+        let rowMap = {}
+        let colMap = {}
+        let boxMap = {}
+        for(let j=0; j<board[i].length;j++){
 
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            if (board[i][j] !== ".") {
-                if (row[i].has(board[i][j])) return false;
-                if (col[j].has(board[i][j])) return false;
-                if (box[getBoxNum(i, j)].has(board[i][j])) return false;
-
-                row[i].add(board[i][j]);
-                col[j].add(board[i][j]);
-                box[getBoxNum(i, j)].add(board[i][j]);
+            let box = board[3* Math.floor(i/3) +Math.floor(j/3)][3*(i%3)+(j%3)]
+            
+            if(board[i][j] != '.'){
+                if(rowMap[board[i][j]]) return false;
+                rowMap[board[i][j]] = 1;
+            }
+            if(board[j][i] != '.') {
+                if(colMap[board[j][i]]) return false;
+                colMap[board[j][i]] = 1;
+            }
+            if(box!= '.'){
+                if(boxMap[box]) return false;
+                boxMap[box] = 1;
             }
         }
     }
-
     return true;
 };
-
-function getBoxNum(i, j) {
-    return 3 * Math.floor(i/3) + Math.floor(j/3);
-}
