aebb044dc632b77ecfb944926c627bcb616e0d55
Minhee-Jeon <58028527+Minhee-Jeon@users.noreply.github.com>
2025-07-25 22:04:38 +0900

    Time: 12 ms (6.6%), Space: 65.3 MB (90.08%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,9 @@
+function maxProfit(prices: number[]): number {
+    let profit = 0;
+    let minPrice = prices[0];
+    prices.forEach((price) => {
+        minPrice = Math.min(minPrice, price);
+        profit = Math.max(profit, price - minPrice);
+    })
+    return profit
+};
